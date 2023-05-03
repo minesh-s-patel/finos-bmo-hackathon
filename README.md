31859276dc0cd96490d03a9754bf5c63b7e34708
Minesh Patel <minesh.patel@regnosys.com>
2023-05-03 13:10:15 +0100

    Started to add some functions

--- README.md
+++ README.md
@@ -16,4 +16,11 @@ Usecase:
 High level plan
 ===============
 
-Write some TS code that processes the json files and attempts to solve the use cases.
+1. Load the JSON files from the use case into code
+2. Write some simple functions to extract some data and print it
+3. Write some functions that compare a couple ECS' and print the diff
+4. Load up a FDC3 reference  implementation (USE SAIL!)
+    -  See https://github.com/finos/FDC3-Sail
+5. Create a custom app directly
+6. Figure out how to add create an app and make a hello world window
+7. Integrate the TC from steps 1-3 and add into the UI
--- index.ts
+++ index.ts
@@ -1,7 +1,42 @@
-const world = 'world';
+import * as fs from 'fs';
 
-export function hello(who: string = world): string {
-  return `Hello ${who}! `;
+export function loadSample(filename: string): any {
+  const jsonString = fs.readFileSync('ecs-json/' + filename, 'utf-8');
+  return JSON.parse(jsonString)
 }
 
-console.log(hello("Finos"));
+/**
+ * Assume we have no multiple cardinality for now - we probably need to deal with it
+ */
+export function assetCountryOfOrigin(sample: any): any {
+  return sample.criteria[0].asset[0].assetCountryOfOrigin[0].value
+}
+
+export function assetType(sample: any): any {
+  return sample.criteria[0].asset[0].collateralAssetType[0].assetType[0]
+}
+
+export function securityType(sample: any): any {
+  return sample.criteria[0].asset[0].collateralAssetType[0].securityType[0]
+}
+
+export function maturityLowerBound(sample: any): any {
+  return sample.criteria[0].asset[0].maturityRange.lowerBound.period.periodMultiplier + sample.criteria[0].asset[0].maturityRange.lowerBound.period.period[0]
+}
+
+export function maturityUpperBound(sample: any): any {
+  return sample.criteria[0].asset[0].maturityRange.upperBound.period.periodMultiplier + sample.criteria[0].asset[0].maturityRange.upperBound.period.period[0]
+}
+
+const EU1515pct = loadSample('EU-1-5-15pct.JSON');
+const US1510pct = loadSample('US-1-5-10pct.JSON');
+const US154pctCorp = loadSample('US-1-5-4pct-Corp.JSON');
+const USCash = loadSample('US-Cash.JSON');
+const EU152pct = loadSample('EU-1-5-2pct.JSON');
+const US152pct = loadSample('US-1-5-2pct.JSON');
+const US154pct = loadSample('US-1-5-4pct.JSON');
+ 
+console.log(maturityUpperBound(EU1515pct));
+
+console.log(maturityUpperBound(EU1515pct));
+
